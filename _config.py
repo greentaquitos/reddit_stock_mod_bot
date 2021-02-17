341cf076d2e87990a9623cf5269c3643b9e8d7fa
Tristan Hanson-Suggitt <tristan@wataq.co>
2021-02-17 12:55:55 -0800

    replace config file with template

---
+++ _config.py
@@ -0,0 +1,19 @@
+
+# email to send big error reports to, or None to turn this off (not set up)
+NOTIFY = ""
+# NOTIFY = None
+
+# subreddit for the bot to moderate
+SUBREDDIT = ""
+
+# bot info from its reddit settings page
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+
+# normal bot login info
+BOT_NAME = ""
+BOT_PASSWORD = ""
+
+# description of bot to label its request for reddit admin
+USER_AGENT = "subreddit manager for /r/"+SUBREDDIT
+
